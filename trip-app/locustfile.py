60c7dc0d990aef8004b7e421e6a7880959a01771
JUNHO0712 <jeff070@naver.com>
2026-04-02 15:03:46 +0900

    add otlp setting code

--- trip-app/locustfile.py
+++ trip-app/locustfile.py
@@ -3,6 +3,24 @@ from locust import HttpUser, task, between
 class TripUser(HttpUser):
     wait_time = between(1, 3)
 
-    @task
-    def test_api(self):
-        self.client.get("/")
+    # ✅ 정상 요청
+    @task(3)
+    def get_trip(self):
+        self.client.get("/api/trips")
+
+    # ❗ 404 에러 유도
+    @task(1)
+    def wrong_url(self):
+        self.client.get("/wrong-url")
+
+    # ❗ 400 에러 유도 (필수값 누락)
+    @task(1)
+    def bad_request(self):
+        self.client.post("/api/trips", json={})
+
+    # ❗ 상태코드 체크해서 강제 실패 처리
+    @task(1)
+    def check_fail(self):
+        with self.client.get("/api/trips", catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("error 발생")
